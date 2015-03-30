f3ce8d4b83523840951b481773e84401a152f7a8
Ricky Ayoub <vgmoose@gmail.com>
2015-03-30 19:40:48 -0400

    Initial Commit

---
+++ SimpDict/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpDict
+//
+//  Created by Ricky Ayoub on 3/30/15.
+//  Copyright (c) 2015 Ricky Ayoub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimpDict/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpDict
+//
+//  Created by Ricky Ayoub on 3/30/15.
+//  Copyright (c) 2015 Ricky Ayoub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
