f0e17456bf5d93c08205aab5fad6398802d97284
Ricky Ayoub <vgmoose@gmail.com>
2015-05-31 13:08:07 -0400

    auto focus on text field on app launch

--- SimpDict/ViewController.m
+++ SimpDict/ViewController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // focus on the textfield
+    [_textField becomeFirstResponder];
 }
 - (IBAction)goPress:(id)sender {
     UIReferenceLibraryViewController* ref;
